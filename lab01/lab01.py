d0df8687dbc03aff886c7a670f1187aab6b0b9a1
shanghaox <xhxie1@stu.suda.edu.cn>
2022-05-21 11:01:55 +0800

    lab01 is donw

---
+++ lab01/lab01.py
@@ -0,0 +1,83 @@
+from itertools import count
+import re
+
+
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if k >= 0:
+        k=k-1
+        return n * falling(n - 1, k)
+
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    # sum=0
+    # while (sum//10!=0):
+    #     sum+=sum%10;
+    #     y=y//10;
+    # return sum
+    
+    if (y//10==0):
+        # return sum_digits(y)
+        return y
+    return sum_digits(y//10)+sum_digits(y%10)
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    # count=0
+    # if (n%10==8):
+    #     count+=1
+    # n=n//10;
+    # if (n//10==0):##如果只剩一位
+    #     if (n%10==8):##最后一位是8
+    #         return count+1;
+    #     else:
+    #         return count;
+    count=0
+    while(n//10!=0):
+        if (n%10==8):
+            count+=1
+        n//=10;
+    if(n%10==8):
+        return count+1==2
+    else:
+        return count==2
